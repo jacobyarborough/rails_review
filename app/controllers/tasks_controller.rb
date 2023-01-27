6dccd9a2249f160a5639f231ccacf2e89370390e
Jacob Yarborough <jayarborough95@gmail.com>
2023-01-26 19:14:19 -0800

    Create tasks new route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController 
     def index 
         @tasks = ["Task 1", "Task 2", "Task 3"]
-    end 
+    end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/', to: "welcome#index"
-  get '/tasks', to: "tasks#index"
+  get "/", to: "welcome#index"
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
 end
