b8a940ff1966746ea424452c4b220adb02b3590d
Jacob Yarborough <jayarborough95@gmail.com>
2023-01-28 10:26:43 -0800

    Refactor laundry? method for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   def laundry?
-    return true if title.downcase.include?("laundry")
+    return true if title.downcase.include?("laundry") || description.downcase.include?("laundry")
+    return false
   end 
 end 
