c60bbd66ecb128ed66ec6189871fde2634f76eec
Jacob Yarborough <jayarborough95@gmail.com>
2023-01-28 10:24:59 -0800

    Refactor laundry? test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe Task, type: :model do
   describe "instance methods" do 
     describe "#laundry?" do 
       it "returns a boolean based on whether or not laundry is in the title or description" do 
-        task = Task.create!(title: "Laundry", description: "Clean clothes")
+        task1 = Task.create!(title: "Laundry", description: "Clean clothes")
+        task2 = Task.create!(title: "Grocery Shopping", description: "Get groceries")
+        task3 = Task.create!(title: "Clean House", description: "Vacuum floors, do laundry, clean room")
 
-        expect(task.laundry?).to be(true)
+        expect(task1.laundry?).to be(true)
+        expect(task2.laundry?).to be(false)
+        expect(task3.laundry?).to be(true)
       end
     end 
   end 
